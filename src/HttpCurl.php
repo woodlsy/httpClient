eddf50f7b104dee3162f745caca5c3bd0eff6d86
叶林森 <yelinsen@jiayouhui360.com>
2019-08-28 11:32:55 +0800

    增加header设置

--- src/HttpCurl.php
+++ src/HttpCurl.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace woodlsy\httpClient;
 
 class HttpCurl
@@ -18,10 +19,7 @@ class HttpCurl
      */
     protected $isHttps = false;
 
-    /**
-     * @var int 超时时间
-     */
-    private $timeOut = 20;
+    protected $header = [];
 
     /**
      * 设置请求链接
@@ -32,7 +30,7 @@ class HttpCurl
      */
     public function setUrl(string $url) : HttpCurl
     {
-        $this->url = $url;
+        $this->url     = $url;
         $this->isHttps = preg_match('/^https?:/i', $url) > 0 ? true : false;
         return $this;
     }
@@ -75,6 +73,19 @@ class HttpCurl
     }
 
     /**
+     * 设置header
+     *
+     * @author yls
+     * @param $str
+     * @return $this
+     */
+    public function setHeader($str)
+    {
+        $this->header[] = $str;
+        return $this;
+    }
+
+    /**
      * curl
      *
      * @author woodlsy
@@ -89,38 +100,30 @@ class HttpCurl
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        if(true === $this->isHttps){
+        if (true === $this->isHttps) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
         }
 
-        if($type == 'POST'){
+        if ($type == 'POST') {
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, is_array($this->data) ? http_build_query($this->data) : $this->data);
         }
-        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeOut);
+
+        if (!empty($this->header)) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
+        }
+
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
         $result = curl_exec($ch);
-        if(false === $result){
+        if (false === $result) {
             throw new HttpClientException(curl_error($ch));
         }
         $info = curl_getinfo($ch);
-        if($info['http_code'] != '200'){
-            throw new HttpClientException('curl status:'.$info['http_code']);
+        if ($info['http_code'] != '200') {
+            throw new HttpClientException('curl status:' . $info['http_code']);
         }
         return $result;
-    }
-
-    /**
-     * 设置超时时间，单位秒
-     *
-     * @author woodlsy
-     * @param int $timeOut
-     * @return HttpCurl
-     */
-    public function timeOut(int $timeOut) : HttpCurl
-    {
-        $this->timeOut = $timeOut;
-        return $this;
     }
 
 }
