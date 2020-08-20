3fcb7bc9910fc81e034cdb8b76bdc37036745220
yls <yelinsen@mayitanlu.cn>
2020-08-20 10:05:34 +0800

    保留数据格式

--- src/HttpCurl.php
+++ src/HttpCurl.php
@@ -39,6 +39,11 @@ class HttpCurl
     protected $result = null;
 
     /**
+     * @var bool 保持数据data原有格式
+     */
+    protected $keepDataFormat = false;
+
+    /**
      * 设置请求链接
      *
      * @author woodlsy
@@ -113,7 +118,7 @@ class HttpCurl
     public function setSSLCert(string $cert, string $key)
     {
         $this->sslCert = $cert;
-        $this->sslKey = $key;
+        $this->sslKey  = $key;
         return $this;
     }
 
@@ -138,6 +143,46 @@ class HttpCurl
     public function getResult()
     {
         return $this->result;
+    }
+
+    /**
+     * 设置数据格式是否保留
+     *
+     * @author yls
+     * @param bool $keep
+     * @return $this
+     */
+    public function setKeepDataFormat(bool $keep)
+    {
+        $this->keepDataFormat = $keep;
+        return $this;
+    }
+
+    /**
+     * 随机IP，只是简单的伪造，最好还是通过代理
+     *
+     * @author yls
+     * @return $this
+     */
+    public function randIp()
+    {
+        $ipLong         = array(
+            array('607649792', '608174079'), //36.56.0.0-36.63.255.255
+            array('1038614528', '1039007743'), //61.232.0.0-61.237.255.255
+            array('1783627776', '1784676351'), //106.80.0.0-106.95.255.255
+            array('2035023872', '2035154943'), //121.76.0.0-121.77.255.255
+            array('2078801920', '2079064063'), //123.232.0.0-123.235.255.255
+            array('-1950089216', '-1948778497'), //139.196.0.0-139.215.255.255
+            array('-1425539072', '-1425014785'), //171.8.0.0-171.15.255.255
+            array('-1236271104', '-1235419137'), //182.80.0.0-182.92.255.255
+            array('-770113536', '-768606209'), //210.25.0.0-210.47.255.255
+            array('-569376768', '-564133889'), //222.16.0.0-222.95.255.255
+        );
+        $rand_key       = mt_rand(0, 9);
+        $ip             = long2ip(mt_rand($ipLong[$rand_key][0], $ipLong[$rand_key][1]));
+        $this->header[] = 'CLIENT-IP:' . $ip;
+        $this->header[] = 'X-FORWARDED-FOR:' . $ip;
+        return $this;
     }
 
     /**
@@ -176,7 +221,7 @@ class HttpCurl
 
         if ($type == 'POST') {
             curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, is_array($this->data) ? http_build_query($this->data) : $this->data);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, is_array($this->data) && !$this->keepDataFormat ? http_build_query($this->data) : $this->data);
         }
 
         if (!empty($this->header)) {
@@ -189,11 +234,11 @@ class HttpCurl
             throw new HttpClientException(curl_error($ch));
         }
         $this->result = $result;
-        $info = curl_getinfo($ch);
+        $info         = curl_getinfo($ch);
         if (301 === (int) $info['http_code']) {
             return $this->setUrl($info['redirect_url'])->fetch($type);
-        }elseif (200 !== (int)$info['http_code']) {
-            return 'curl status:' . $info['http_code'] .' curl url:'.$this->url;
+        } elseif (200 !== (int) $info['http_code']) {
+            return 'curl status:' . $info['http_code'] . ' curl url:' . $this->url;
         }
         return $result;
     }
